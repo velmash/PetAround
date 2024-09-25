c1738ad6091595f774eb093aad9c0a573823df20
velmash <hyengchan@gmail.com>
2024-09-25 19:41:18 +0900

    :tada: 프로젝트 생성

---
+++ PetAround/PetAroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetAroundApp.swift
+//  PetAround
+//
+//  Created by 윤형찬 on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct PetAroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PetAroundTests/PetAroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  PetAroundTests.swift
+//  PetAroundTests
+//
+//  Created by 윤형찬 on 9/25/24.
+//
+
+import Testing
+@testable import PetAround
+
+struct PetAroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
