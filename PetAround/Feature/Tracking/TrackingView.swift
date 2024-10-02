e5fabf409b010dca21ed62d0986d23fc600cf265
윤형찬 <72960152+velmash@users.noreply.github.com>
2024-10-02 17:53:38 +0900

    :wrench: 산책 쪽 BaseUI 구현

--- PetAround/Feature/Tracking/TrackingView.swift
+++ PetAround/Feature/Tracking/TrackingView.swift
@@ -13,13 +13,44 @@ struct TrackingView: View {
             Color(.bg)
                 .edgesIgnoringSafeArea(.top)
             
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
+            VStack(alignment: .leading, spacing: 40) {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(Color.white)
+                        .strokeBorder(Color(.gray200), lineWidth: 1.5)
+                    
+                    HStack(spacing: 0) {
+                        Text("산책 기록")
+                            .font(.system(size: 20, weight: .semibold))
+                            
+                        Spacer()
+                        
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color.white)
+                                .strokeBorder(Color(.gray200), lineWidth: 1.5)
+                            
+                            Button {
+                                print("HI")
+                            } label: {
+                                Text("시작")
+                            }
+                        }
+                        .frame(width: 80)
+                        .padding(.vertical, 15)
+                    }
+                    .padding(.horizontal, 16)
+                }
+                .frame(height: 80)
+                
+                Text("산책 히스토리")
+                    .font(.system(size: 18, weight: .semibold))
+                
+                Spacer()
+                
             }
-            .padding()
+            .padding(.top, 20)
+            .padding(.horizontal, 20)
         }
     }
 }
